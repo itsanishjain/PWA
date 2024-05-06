b564e3aa401950699a2bc36267b3a55abc7514a9
eldante <dan.tgw@gmail.com>
2024-05-06 13:52:52 +0800

    Update constants to read from environment variable.

--- constants/constant.tsx
+++ constants/constant.tsx
@@ -66,7 +66,10 @@ export const mainChain = base
 // 		default: { name: 'Explorer', url: 'https://basescan.org' },
 // 	},
 // })
-export const network: Network = Network.Testnet
+export const network: Network =
+	process.env.NEXT_PUBLIC_ENV === 'production'
+		? Network.Mainnet
+		: Network.Testnet
 
 export const localnetTokenAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
 export const localnetContractAddress =
