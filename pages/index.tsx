078d9bd3c28e725409bdeff1c38493f5f83a214b
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-05-30 06:51:23 -0500

    chore: clean home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,9 @@
 import Page from '@/components/page'
 import Section from '@/components/section'
-import Image from 'next/image'
-import poolImage from '@/public/images/pool.png'
-import { useRouter } from 'next/router'
-import {
-	UnsignedTransactionRequest,
-	usePrivy,
-	useWallets,
-} from '@privy-io/react-auth'
-
-import React, { useState, useEffect } from 'react'
-
-import { ethers } from 'ethers'
 import Appbar from '@/components/appbar'
-
-import { Inter } from 'next/font/google'
-import styles from './styles/index.module.css'
-import PoolRow from '@/components/poolRow'
 import UpcomingPoolTab from '@/components/tabs/UpcomingPoolTab'
-import PastPoolTab from '@/components/tabs/PastPoolTab'
-
-const inter = Inter({ subsets: ['latin'] })
 
 const App = () => {
-	const router = useRouter()
-	const { ready, authenticated, user, signMessage, sendTransaction, logout } =
-		usePrivy()
-
-	const { wallets } = useWallets()
-
-	// Replace this with the message you'd like your user to sign
-
-	// Replace this with the text you'd like on your signature modal,
-	// if you do not have `noPromptsOnSignature` enabled
-
-	const [selectedTab, setSelectedTab] = useState(0)
-	const selectTab = (tabIndex: number) => {
-		setSelectedTab(tabIndex)
-	}
-
-	useEffect(() => {
-		// Update the document title using the browser API
-		if (wallets.length > 0) {
-			console.log(`Wallet Length: ${wallets.length}`)
-			// console.log(`Wallet Address: ${wallets[0].address}`)
-		}
-		for (var i = 0; i < wallets.length; i++) {
-			console.log(`Wallet ${i} Address: ${wallets[i].address}`)
-		}
-	}, [wallets])
-
 	return (
 		<Page>
 			<Appbar />
