02baade78c1bc3d68137efc5923554eb35ee4c02
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-01 03:36:20 +0200

    fix: remove magic address and use configured one

--- src/app/pwa/@content/profile/@info/page.tsx
+++ src/app/pwa/@content/profile/@info/page.tsx
@@ -7,17 +7,21 @@ import { useUserStore } from '@/stores/profile.store'
 
 import SponsoredTxn from '@/components/sponsored-txn/sponsored-txn-CoinbaseSmartWallet'
 import { dropletAbi, dropletAddress } from '@/types/droplet'
+import { wagmi } from '@/providers/configs'
+import { useRouter } from 'next/navigation'
+import { Route } from 'next'
 
 export default function ProfileHeader() {
     const account = useAccount()
     const { profile } = useUserStore()
+    const router = useRouter()
 
     if (!account.address) {
-        // TODO: redirect at this point?
-        return null
+        // router.push('/' as Route)
+        console.log('No account address found in the profile!')
     }
 
-    const truncatedAddress = account.address.slice(0, 6) + '...' + account.address.slice(-4)
+    const truncatedAddress = account.address?.slice(0, 6) + '...' + account.address?.slice(-4)
 
     return (
         <header className='flex-center mt-4 flex-col gap-4'>
@@ -28,7 +32,7 @@ export default function ProfileHeader() {
             <div>{truncatedAddress}</div>
             <SponsoredTxn
                 text='Mint 1000 DROPLET'
-                targetAddress='0xfD2Ec58cE4c87b253567Ff98ce2778de6AF0101b'
+                targetAddress={dropletAddress[wagmi.config.state.chainId as ChainId]}
                 abi={dropletAbi}
                 functionName='mint'
                 args={[account.address, '1000000000000000000000']}
