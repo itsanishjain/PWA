22861805f809994abf1793053be5d42805e5dd4e
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-01 03:36:38 +0200

    fix: missing type for route

--- src/app/pwa/@content/profile/@main/claim-winning/page.tsx
+++ src/app/pwa/@content/profile/@main/claim-winning/page.tsx
@@ -1,10 +1,11 @@
+import { Route } from 'next'
 import Link from 'next/link'
 
 export default function RequestRefundPage() {
     return (
         <div>
             <h1>Claim winning Page</h1>
-            <Link href='/profile'>Go back to profile</Link>
+            <Link href={'/profile' as Route}>Go back to profile</Link>
         </div>
     )
 }
