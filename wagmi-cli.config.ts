7cfbaf9bfd985c4bfa9d0b03090e2158e8965cae
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-07 21:13:34 +0200

    chore: organize wagmi config and separate deployments

--- wagmi-cli.config.ts
+++ wagmi-cli.config.ts
@@ -1,21 +1,49 @@
 import { defineConfig } from '@wagmi/cli'
-import { foundry } from '@wagmi/cli/plugins'
+import { foundry, react } from '@wagmi/cli/plugins'
+import type { Address } from 'viem'
+import { erc20Abi } from 'viem'
 import { baseSepolia } from 'viem/chains'
+
+const poolDeployments = {
+    [baseSepolia.id]: '0x5C22662210E48D0f5614cACA6f7a6a938716Ea26' as Address, // github commit: eda48eb,
+}
+
+const dropletDeployments = {
+    [baseSepolia.id]: '0xfD2Ec58cE4c87b253567Ff98ce2778de6AF0101b' as Address,
+}
+
+const dropletAbi = [
+    ...erc20Abi,
+    {
+        type: 'function',
+        inputs: [
+            { name: 'to', internalType: 'address', type: 'address' },
+            { name: 'amount', internalType: 'uint256', type: 'uint256' },
+        ],
+        name: 'mint',
+        outputs: [],
+        stateMutability: 'nonpayable',
+    },
+] as const
 
 export default defineConfig({
     out: 'src/types/contracts.ts',
+    contracts: [
+        {
+            name: 'Droplet',
+            abi: dropletAbi,
+            address: dropletDeployments,
+        },
+    ],
     plugins: [
         foundry({
             project: 'contracts',
-            include: ['Droplet.json', 'Pool.json'],
+            include: ['Pool.json'],
             deployments: {
-                Pool: {
-                    [baseSepolia.id]: '0x5C22662210E48D0f5614cACA6f7a6a938716Ea26', // github commit: eda48eb
-                },
-                Droplet: {
-                    [baseSepolia.id]: '0xfD2Ec58cE4c87b253567Ff98ce2778de6AF0101b',
-                },
+                Pool: poolDeployments,
+                Droplet: dropletDeployments,
             },
         }),
+        react(),
     ],
 })
