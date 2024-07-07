26deefdcdadb3732ebf32571b8650a0fdaa0d45c
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-07 21:09:37 +0200

    tests: disable unit tests for now

--- tests/mock-request.ts
+++ tests/mock-request.ts
@@ -1,18 +1,18 @@
-// src/lib/tests/mock-request.ts
-import { NextRequest } from 'next/server'
+// // src/lib/tests/mock-request.ts
+// import type { NextRequest } from 'next/server'
 
-export function createMockedRequest(
-    options: {
-        pathname?: string
-        cookies?: { [key: string]: string }
-    } = {},
-) {
-    const req = {
-        nextUrl: { pathname: options.pathname || '/' },
-        cookies: {
-            get: (name: string) => ({ value: options.cookies?.[name] }),
-        },
-    } as unknown as NextRequest
+// export function createMockedRequest(
+//     options: {
+//         pathname?: string
+//         cookies?: { [key: string]: string }
+//     } = {},
+// ) {
+//     const req = {
+//         nextUrl: { pathname: options.pathname || '/' },
+//         cookies: {
+//             get: (name: string) => ({ value: options.cookies?.[name] }),
+//         },
+//     } as unknown as NextRequest
 
-    return { req }
-}
+//     return { req }
+// }
