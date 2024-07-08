0fe1f77766620efa4d5ffab0c18f9245f86d5625
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-08 23:55:52 +0200

    refactor: Add getSession function for retrieving session data
    The `getSession` function is added to retrieve session data from the server. This function makes an API call to '/api/onramp_stripe' and returns the session response. If there is an error, it logs the error and returns null.
    
    Co-authored-by: eldante <dan.tgw@gmail.com>
    Co-authored-by: Zk <zklim95@hotmail.com>

---
+++ src/app/pwa/(pages)/profile/_components/onramps/get-session.ts
@@ -0,0 +1,18 @@
+interface SessionResponse {
+    client_secret: string | null
+}
+
+export const getSession = async (): Promise<SessionResponse> => {
+    try {
+        const response = await fetch('/api/onramp_stripe')
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        const data = (await response.json()) as SessionResponse
+
+        return data
+    } catch (error) {
+        console.error('There was a problem with the post operation:', error)
+        return { client_secret: null }
+    }
+}
