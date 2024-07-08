29c79b5b4a59a4563de001310e38fc2ed7e16dee
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-08 23:56:07 +0200

    refactor: Update dictionaryToArray function to handle null input
    The `dictionaryToArray` function is modified to handle null input. If the `dictionary` parameter is null, the function now returns an empty array. This change improves the robustness of the function.
    
    Co-authored-by: eldante <dan.tgw@gmail.com>
    Co-authored-by: Zk <zklim95@hotmail.com>

--- src/app/pwa/_lib/utils/database.ts
+++ src/app/pwa/_lib/utils/database.ts
@@ -1,48 +1,63 @@
 interface Dictionary {
-	[key: number]: string
+    [key: number]: string
 }
 export function dictionaryToArray(dictionary: Dictionary): string[] {
-	if (dictionary == null) {
-		return []
-	}
-	return Object.keys(dictionary).map((key) =>
-		dictionary[parseInt(key)].toLowerCase(),
-	)
+    if (dictionary == null) {
+        return []
+    }
+    return Object.keys(dictionary).map(key => dictionary[parseInt(key)].toLowerCase())
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function dictionaryToNestedArray(dictionary: any): any[][] | null {
-	if (dictionary == null) {
-		return null
-	}
-	return Object.keys(dictionary).map((key) => dictionary[parseInt(key)])
+    if (dictionary == null) {
+        return null
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+    return Object.keys(dictionary).map(key => dictionary[parseInt(key)])
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getAllIndicesMatching(arr: any, value: any) {
-	return arr?.reduce((indices: any, element: any, index: any) => {
-		if (element === value) {
-			indices.push(index)
-		}
-		return indices
-	}, [])
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    return arr?.reduce((indices: any, element: any, index: any) => {
+        if (element === value) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            indices.push(index)
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return indices
+    }, [])
 }
 
 export function getRowIndicesByColumnValue(
-	arr2D: any,
-	columnIndex: any,
-	value: any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    arr2D: any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    columnIndex: any,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    value: any,
 ) {
-	const rowIndices: any[] = []
-	arr2D.forEach((row: any, rowIndex: any) => {
-		if (row[columnIndex] === value) {
-			rowIndices.push(rowIndex)
-		}
-	})
-	return rowIndices
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const rowIndices: any[] = []
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    arr2D.forEach((row: any, rowIndex: any) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        if (row[columnIndex] === value) {
+            rowIndices.push(rowIndex)
+        }
+    })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return rowIndices
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getRowsByColumnValue(arr2D: any, columnIndex: any, value: any) {
-	return arr2D?.filter((row: any) => row[columnIndex] === value)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    return arr2D?.filter((row: any) => row[columnIndex] === value)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getValuesFromIndices(arr: any, indices: any) {
-	return indices?.map((index: any) => arr[index])
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    return indices?.map((index: any) => arr[index])
 }
