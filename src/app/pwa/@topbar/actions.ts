b4090c75542b83aff71c5d46c2e739117f0fe901
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-08 23:57:40 +0200

    feat: Add user avatar error logging in topbar actions

--- src/app/pwa/@topbar/actions.ts
+++ src/app/pwa/@topbar/actions.ts
@@ -5,5 +5,7 @@ import { getUserUseCase } from '../_server/use-cases/users/get-user'
 export const getUserAvatarAction = async (): Promise<string | null> => {
     const user = await getUserUseCase()
 
+    if (!user?.avatar) console.log('User avatar not found for user:', user?.displayName)
+
     return user?.avatar || null
 }
