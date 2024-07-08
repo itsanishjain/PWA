de62a191b916def16b6e12563b445de0fe7a9751
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-08 23:53:43 +0200

    refactor: update avatars component to use custom Avatar component
    The `avatars.tsx` component has been updated to use the custom `Avatar` component from the `ui/avatar` module. This change improves code organization and reusability.

--- src/app/pwa/(pages)/pool/[pool-id]/_components/avatars.tsx
+++ src/app/pwa/(pages)/pool/[pool-id]/_components/avatars.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/app/pwa/_components/ui/avatar'
 import { cn } from '@/lib/utils/tailwind'
-import Image from 'next/image'
 
 interface AvatarsProps {
     className?: string
@@ -13,21 +13,31 @@ const Avatars = ({ numPeople, className, avatarUrls }: AvatarsProps) => {
     return (
         <div className={cn('z-10 flex -space-x-2 rtl:space-x-reverse', className)}>
             {avatarUrls.map((url, index) => (
-                <Image
+                // <Image
+                //     key={index}
+                //     className='h-10 w-10 rounded-full border-2 border-white dark:border-gray-800'
+                //     src={url}
+                //     width={40}
+                //     height={40}
+                //     alt={`Avatar ${index + 1}`}
+                //     priority
+                // />
+
+                <Avatar
                     key={index}
-                    className='h-10 w-10 rounded-full border-2 border-white dark:border-gray-800'
-                    src={url}
-                    width={40}
-                    height={40}
-                    alt={`Avatar ${index + 1}`}
-                    priority
-                />
+                    className='size-10 rounded-full border-2 border-white dark:border-gray-800'
+                    aria-label='User Avatar'>
+                    <AvatarImage alt={`Avatar ${index + 1}`} src={url} />
+                    <AvatarFallback className='bg-[#d9d9d9]' />
+                </Avatar>
             ))}
-            <a
-                className='flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-[#5E6B80] text-center text-xs font-bold text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black'
-                href=''>
-                +{numPeople}
-            </a>
+            {(numPeople ?? 0) >= 5 && (
+                <a
+                    className='z-10 flex size-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black'
+                    href=''>
+                    +{numPeople! - 4}
+                </a>
+            )}
         </div>
     )
 }
