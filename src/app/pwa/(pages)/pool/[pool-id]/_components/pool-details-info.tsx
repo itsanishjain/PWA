13b306499370a6bd8a64813e0e20d36a400d2334
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-09 05:27:30 +0200

    style: remove grey bg from pool info

--- src/app/pwa/(pages)/pool/[pool-id]/_components/pool-details-info.tsx
+++ src/app/pwa/(pages)/pool/[pool-id]/_components/pool-details-info.tsx
@@ -30,7 +30,7 @@ export default function PoolDetailsInfo({ description, price, tokenSymbol, terms
     ]
 
     return (
-        <div className='space-y-3 rounded-[2.875rem] bg-[#F4F4F4] p-5'>
+        <div className='space-y-3 rounded-[2.875rem] p-5'>
             {items.map((item, index) => (
                 <PoolDetailsInfoItem key={index} title={item.title} value={item.value} />
             ))}
