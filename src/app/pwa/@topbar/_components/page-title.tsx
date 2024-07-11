4537ebf547199498a420f95e8e17a4641bc99578
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-12 00:48:19 +0200

    feat: click logo to go back home

--- src/app/pwa/@topbar/_components/page-title.tsx
+++ src/app/pwa/@topbar/_components/page-title.tsx
@@ -2,11 +2,16 @@
 
 import { useSettingsStore } from '@/app/pwa/_client/providers/settings.provider'
 import PoolTopLogo from '@/app/pwa/_components/icons/pool-top-logo'
+import { Route } from 'next'
+import Link from 'next/link'
 
 export default function PageTitle() {
     const topBarTitle = useSettingsStore(state => state.topBarTitle)
 
-    return <h1 className='text-[0.875rem] font-semibold'>{topBarTitle ? topBarTitle : <PoolTopLogo />}</h1>
+    return
+    ;<Link href={'/' as Route}>
+        <h1 className='text-[0.875rem] font-semibold'>{topBarTitle ? topBarTitle : <PoolTopLogo />}</h1>
+    </Link>
     // Alternative with text instead of svg/png:
     // <h1 className='text-center text-[42px] font-bold text-shadow-inner font-logo bg-clip-text text-transparent bg-text-inner'>
     // 	pool
