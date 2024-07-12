ed187d8c9ea2e810c6a9e2453141af67cbd0731a
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-07-12 02:57:33 +0200

    fix: remove typo from pagetitle preventing re-render

--- src/app/pwa/@topbar/_components/page-title.tsx
+++ src/app/pwa/@topbar/_components/page-title.tsx
@@ -8,10 +8,11 @@ import Link from 'next/link'
 export default function PageTitle() {
     const topBarTitle = useSettingsStore(state => state.topBarTitle)
 
-    return
-    ;<Link href={'/' as Route}>
-        <h1 className='text-[0.875rem] font-semibold'>{topBarTitle ? topBarTitle : <PoolTopLogo />}</h1>
-    </Link>
+    return (
+        <Link href={'/' as Route}>
+            <h1 className='text-[0.875rem] font-semibold'>{topBarTitle ? topBarTitle : <PoolTopLogo />}</h1>
+        </Link>
+    )
     // Alternative with text instead of svg/png:
     // <h1 className='text-center text-[42px] font-bold text-shadow-inner font-logo bg-clip-text text-transparent bg-text-inner'>
     // 	pool
