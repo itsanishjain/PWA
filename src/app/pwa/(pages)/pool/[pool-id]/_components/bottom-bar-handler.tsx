2d7176663400bdbbfb1f494d2a245af13cee553b
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-08-04 14:22:52 -0400

    feat: show ticket link when user is registered

--- src/app/pwa/(pages)/pool/[pool-id]/_components/bottom-bar-handler.tsx
+++ src/app/pwa/(pages)/pool/[pool-id]/_components/bottom-bar-handler.tsx
@@ -1,16 +1,14 @@
 'use client'
 
 import { Button } from '@/app/pwa/_components/ui/button'
-import { poolAbi, useReadDropletBalanceOf } from '@/types/contracts'
+import { useReadPoolIsParticipant } from '@/types/contracts'
 import { useEffect } from 'react'
-import { toast } from 'sonner'
-import { Address, parseUnits } from 'viem'
-import useSmartTransaction from '@/app/pwa/_client/hooks/use-smart-transaction'
-import { currentPoolAddress } from '@/app/pwa/_server/blockchain/server-config'
+import { Address } from 'viem'
 import { useAppStore } from '@/app/pwa/_client/providers/app-store.provider'
-import { useAuth } from '@/app/pwa/_client/hooks/use-auth'
 import { POOLSTATUS } from '../_lib/definitions'
 import { usePoolActions } from '@/app/pwa/_client/hooks/use-pool-actions'
+import { useRouter } from 'next/navigation'
+import { Route } from 'next'
 
 export default function BottomBarHandler({
     isAdmin,
@@ -29,13 +27,15 @@ export default function BottomBarHandler({
     tokenDecimals: number
     walletAddress: Address | null
 }) {
+    const router = useRouter()
     const { setBottomBarContent } = useAppStore(state => ({
         setBottomBarContent: state.setBottomBarContent,
     }))
 
-    // const { login, authenticated: isAuthenticated } = useAuth()
-    // const { executeTransaction, isReady } = useSmartTransaction()
-    // const { data: userBalance, error: balanceError } = useReadDropletBalanceOf({ args: [walletAddress || '0x'] })
+    // @ts-expect-error ts(2589) FIXME: Type instantiation is excessively deep and possibly infinite.
+    const { data: isParticipant } = useReadPoolIsParticipant({
+        args: [walletAddress || '0x', poolId],
+    })
 
     const { handleEnableDeposits, handleEndPool, handleJoinPool, handleStartPool, ready } = usePoolActions(
         poolId,
@@ -44,95 +44,27 @@ export default function BottomBarHandler({
     )
 
     useEffect(() => {
-        ready && handleBottomBarContent(poolStatus, isAdmin)
+        if (ready) {
+            if (isParticipant) {
+                setBottomBarContent(
+                    <Button
+                        className='mb-3 h-[46px] w-full rounded-[2rem] bg-cta px-6 py-[11px] text-center text-base font-semibold leading-normal text-white shadow-button active:shadow-button-push'
+                        onClick={handleViewTicket}>
+                        View My Ticket
+                    </Button>,
+                )
+            } else {
+                handleBottomBarContent(poolStatus, isAdmin)
+            }
+        }
         return () => {
             setBottomBarContent(null)
         }
-    }, [poolStatus, isAdmin, setBottomBarContent, ready])
+    }, [poolStatus, isAdmin, setBottomBarContent, ready, isParticipant])
 
-    // const handleEnableDeposits = async () => {
-    //     toast('Enabling deposits...')
-
-    //     await executeTransaction([
-    //         {
-    //             address: currentPoolAddress,
-    //             abi: poolAbi,
-    //             functionName: 'enableDeposit',
-    //             args: [poolId],
-    //         },
-    //     ])
-    // }
-
-    // const handleStartPool = async () => {
-    //     toast('Starting pool...')
-
-    //     await executeTransaction([
-    //         {
-    //             address: currentPoolAddress,
-    //             abi: poolAbi,
-    //             functionName: 'startPool',
-    //             args: [poolId],
-    //         },
-    //     ])
-    // }
-
-    // const handleEndPool = async () => {
-    //     toast('Ending pool...')
-
-    //     await executeTransaction([
-    //         {
-    //             address: currentPoolAddress,
-    //             abi: poolAbi,
-    //             functionName: 'endPool',
-    //             args: [poolId],
-    //         },
-    //     ])
-    // }
-
-    // const handleJoinPool = async () => {
-    //     console.log('Join pool button clicked')
-
-    //     if (!isReady) {
-    //         console.log('Wallet not ready')
-    //         return
-    //     }
-
-    //     if (isReady && !isAuthenticated) {
-    //         console.log('Login first')
-    //         login()
-    //     }
-
-    //     if (isReady && isAuthenticated) {
-    //         console.log('Check funds')
-
-    //         const bigIntPrice = parseUnits(poolPrice.toString(), tokenDecimals)
-    //         console.log('Big int price:', bigIntPrice.toString())
-
-    //         if (balanceError) {
-    //             console.error('Error reading balance', balanceError)
-    //             return
-    //         }
-
-    //         if (Number(userBalance || 0) < bigIntPrice) {
-    //             toast('Insufficient funds, please top up your account.')
-    //             return
-    //         }
-
-    //         console.log('Onramp funds if needed')
-
-    //         console.log('Join pool')
-    //         toast('Joining pool...')
-
-    //         await executeTransaction([
-    //             {
-    //                 address: currentPoolAddress,
-    //                 abi: poolAbi,
-    //                 functionName: 'deposit',
-    //                 args: [poolId, bigIntPrice],
-    //             },
-    //         ])
-    //     }
-    // }
+    const handleViewTicket = () => {
+        router.push(`/pool/${poolId}/ticket` as Route)
+    }
 
     const handleBottomBarContent = (poolStatus: POOLSTATUS, isAdmin: boolean) => {
         switch (poolStatus) {
