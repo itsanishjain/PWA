2abd94374a16ad07b1d313712c0a9b427dbbf608
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-08-04 13:20:20 -0400

    fix: remove auth wrapper from pool-details component

--- src/app/pwa/(pages)/pool/[pool-id]/_components/pool-details.tsx
+++ src/app/pwa/(pages)/pool/[pool-id]/_components/pool-details.tsx
@@ -7,73 +7,65 @@ import PoolDetailsBanner from './pool-details-banner'
 import PoolDetailsBannerButtons from './pool-details-banner-button'
 import PoolDetailsBannerStatus from './pool-details-banner-status'
 import PoolDetailsInfo from './pool-details-info'
-import BottomBarHandler from './bottom-bar-handler'
 import { checkAuthStatusAction } from '../../../pools/actions'
 import AuthenticatedContent from './authenticated-pool-details'
-import { Divide } from 'lucide-react'
+import BottomBarHandler from './bottom-bar-handler'
 
 export default async function PoolDetails({ pool }: { pool: PoolDetailsDTO }) {
     const poolBalance = pool.numParticipants * pool.price
     const avatarUrls = pool.participants.map(participant => participant.avatarUrl)
     const [result] = await checkAuthStatusAction()
-    const isAdmin = result && 'isAdmin' in result && result.isAdmin
+    const isAdmin = (result && 'isAdmin' in result && result.isAdmin) || false
     const walletAddress = result && 'address' in result ? result.address : null
 
     return (
-        <>
-            <AuthenticatedContent
+        <div className='space-y-3 bg-white p-2'>
+            <section className='detail_card rounded-[2.875rem] p-[1.12rem]'>
+                <PoolDetailsBanner
+                    name={pool.name}
+                    imageUrl={pool.imageUrl}
+                    buttons={<PoolDetailsBannerButtons isAdmin={isAdmin} />}
+                    status={<PoolDetailsBannerStatus />}
+                />
+                <PoolDetailsHeading
+                    name={pool.name}
+                    startDate={pool.startDate}
+                    endDate={pool.endDate}
+                    hostName={pool.hostName}
+                />
+                <PoolDetailsClaimableWinnings
+                    claimableAmount={pool.claimableAmount}
+                    tokenSymbol={pool.tokenSymbol}
+                    poolId={pool.contractId}
+                />
+
+                <div className='space-y-3 rounded-[2rem] bg-[#F4F4F4] p-5'>
+                    <PoolDetailsProgress current={poolBalance} goal={pool.goal} />
+                    <PoolDetailsParticipants
+                        poolId={pool.contractId.toString()}
+                        numParticipants={pool.numParticipants}
+                        avatarUrls={avatarUrls}
+                    />
+                </div>
+            </section>
+
+            <section className='detail_card space-y-6 rounded-[2.875rem] px-[1.12rem] py-6'>
+                <PoolDetailsInfo
+                    description={pool.description}
+                    price={pool.price}
+                    tokenSymbol={pool.tokenSymbol}
+                    termsUrl={pool.termsUrl || ''}
+                />
+            </section>
+            <BottomBarHandler
+                poolId={pool.contractId}
                 isAdmin={isAdmin}
-                renderBottomBar={
-                    <BottomBarHandler
-                        poolId={pool.contractId}
-                        isAdmin={isAdmin || false}
-                        poolStatus={pool.status}
-                        poolPrice={pool.price}
-                        poolTokenSymbol={pool.tokenSymbol}
-                        tokenDecimals={pool.tokenDecimals}
-                        walletAddress={walletAddress}
-                    />
-                }
+                poolStatus={pool.status}
+                poolPrice={pool.price}
+                poolTokenSymbol={pool.tokenSymbol}
+                tokenDecimals={pool.tokenDecimals}
+                walletAddress={walletAddress}
             />
-            <div className='space-y-3 bg-white p-2'>
-                <section className='detail_card rounded-[2.875rem] p-[1.12rem]'>
-                    <PoolDetailsBanner
-                        name={pool.name}
-                        imageUrl={pool.imageUrl}
-                        buttons={<PoolDetailsBannerButtons isAdmin={isAdmin} />}
-                        status={<PoolDetailsBannerStatus />}
-                    />
-                    <PoolDetailsHeading
-                        name={pool.name}
-                        startDate={pool.startDate}
-                        endDate={pool.endDate}
-                        hostName={pool.hostName}
-                    />
-                    <PoolDetailsClaimableWinnings
-                        claimableAmount={pool.claimableAmount}
-                        tokenSymbol={pool.tokenSymbol}
-                        poolId={pool.contractId}
-                    />
-
-                    <div className='space-y-3 rounded-[2rem] bg-[#F4F4F4] p-5'>
-                        <PoolDetailsProgress current={poolBalance} goal={pool.goal} />
-                        <PoolDetailsParticipants
-                            poolId={pool.contractId.toString()}
-                            numParticipants={pool.numParticipants}
-                            avatarUrls={avatarUrls}
-                        />
-                    </div>
-                </section>
-
-                <section className='detail_card space-y-6 rounded-[2.875rem] px-[1.12rem] py-6'>
-                    <PoolDetailsInfo
-                        description={pool.description}
-                        price={pool.price}
-                        tokenSymbol={pool.tokenSymbol}
-                        termsUrl={pool.termsUrl || ''}
-                    />
-                </section>
-            </div>
-        </>
+        </div>
     )
 }
