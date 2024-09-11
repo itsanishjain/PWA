761fe49a2cee5325f453ed8bd9b1ca9921f5b168
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-11 05:38:12 +0200

    refactor: replace auth guard to use new improved method when creating pool

--- src/app/pwa/(pages)/pool/new/page.tsx
+++ src/app/pwa/(pages)/pool/new/page.tsx
@@ -1,11 +1,10 @@
-import { getAuthStatus } from '@/app/pwa/_server/auth/auth.action'
+import { getAdminStatusAction } from '../../pools/actions'
 import CreatePoolForm from './create-pool-form'
 
 export default async function CreatePoolPage() {
-    const authStatus = await getAuthStatus()
-    const isAuthorized = authStatus.isAuthenticated && authStatus.isAdmin
+    const [isAdmin] = await getAdminStatusAction()
 
-    if (!isAuthorized) {
+    if (!isAdmin) {
         return <div className={'mt-4 w-full text-center'}>You are not authorized to create a pool.</div>
     }
 
