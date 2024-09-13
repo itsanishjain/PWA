2791fe0c352106417366c62e64a9b90e52a101da
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-13 06:50:24 +0200

    chore: format file

--- src/app/pwa/(pages)/pool/[pool-id]/_components/bottom-bar-handler.tsx
+++ src/app/pwa/(pages)/pool/[pool-id]/_components/bottom-bar-handler.tsx
@@ -50,8 +50,16 @@ export default function BottomBarHandler({
         args: [walletAddress || '0x', poolId],
     })
 
-    const { handleEnableDeposits, handleEndPool, handleJoinPool, handleStartPool, ready, isPending, isConfirmed, resetConfirmation } =
-        usePoolActions(poolId, poolPrice, tokenDecimals, () => setOpenOnRampDialog(true))
+    const {
+        handleEnableDeposits,
+        handleEndPool,
+        handleJoinPool,
+        handleStartPool,
+        ready,
+        isPending,
+        isConfirmed,
+        resetConfirmation,
+    } = usePoolActions(poolId, poolPrice, tokenDecimals, () => setOpenOnRampDialog(true))
 
     const handleViewTicket = useCallback(() => {
         router.push(`/pool/${poolId}/ticket` as Route)
@@ -65,7 +73,9 @@ export default function BottomBarHandler({
             },
             [POOLSTATUS.DEPOSIT_ENABLED]: {
                 admin: { label: 'Start Pool', action: handleStartPool },
-                user: isParticipant ? { label: 'View My Ticket', action: handleViewTicket } : { label: `Register for ${poolPrice} ${poolTokenSymbol}`, action: handleJoinPool },
+                user: isParticipant
+                    ? { label: 'View My Ticket', action: handleViewTicket }
+                    : { label: `Register for ${poolPrice} ${poolTokenSymbol}`, action: handleJoinPool },
             },
             [POOLSTATUS.STARTED]: {
                 admin: { label: 'End pool', action: handleEndPool },
@@ -132,5 +142,5 @@ export default function BottomBarHandler({
         }
     }, [isConfirmed, updateBottomBarContent, router, resetConfirmation])
 
-    return <OnRampDialog open={openOnRampDialog} setOpen={setOpenOnRampDialog} amount={poolPrice.toString()}/>
+    return <OnRampDialog open={openOnRampDialog} setOpen={setOpenOnRampDialog} amount={poolPrice.toString()} />
 }
