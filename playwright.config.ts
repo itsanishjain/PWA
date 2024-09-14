2f2b4169f07e7c9b9b29597d53b5be0a6e51ad8f
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-14 06:55:30 +0200

    feat: add basic not_found page

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,30 +33,30 @@ export default defineConfig({
 
     /* Configure projects for major browsers */
     projects: [
-        // {
-        //   name: 'chromium',
-        //   use: { ...devices['Desktop Chrome'] },
-        // },
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
 
-        // {
-        //   name: 'firefox',
-        //   use: { ...devices['Desktop Firefox'] },
-        // },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
 
-        // {
-        //   name: 'webkit',
-        //   use: { ...devices['Desktop Safari'] },
-        // },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
 
         /* Test against mobile viewports. */
-        {
-            name: 'Mobile Chrome',
-            use: { ...devices['Pixel 5'] },
-        },
-        {
-            name: 'Mobile Safari',
-            use: { ...devices['iPhone 13'] },
-        },
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
 
         /* Test against branded browsers. */
         // {
@@ -71,8 +71,8 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     // webServer: {
-    //     command: 'npm run dev',
-    //     url: 'http://app.localhost:3000',
+    //     command: 'pnpm run start',
+    //     url: 'https://127.0.0.1:3000',
     //     reuseExistingServer: !process.env.CI,
     // },
 })
