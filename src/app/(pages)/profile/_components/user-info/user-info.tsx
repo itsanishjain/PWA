5cf7f89d3790fc6940fb494ca793eb62cd082ac5
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-14 21:38:20 +0200

    feat: replace react-blockies with more efficient blo

--- src/app/(pages)/profile/_components/user-info/user-info.tsx
+++ src/app/(pages)/profile/_components/user-info/user-info.tsx
@@ -1,41 +1,35 @@
 'use client'
 
-import { Avatar, AvatarImage } from '@/app/pwa/_components/ui/avatar'
+import { Avatar, AvatarImage } from '@/app/_components/ui/avatar'
 import { ExternalLinkIcon } from 'lucide-react'
 import Link from 'next/link'
 import { useAccount } from 'wagmi'
 import { getUserInfoAction } from '../../actions'
-import { Skeleton } from '@/app/pwa/_components/ui/skeleton'
-import ReactBlockies from 'react-blockies'
+import { Skeleton } from '@/app/_components/ui/skeleton'
 import { Tables } from '@/types/db'
-import { useServerActionQuery } from '@/app/pwa/_client/hooks/server-action-hooks'
+import { useServerActionQuery } from '@/app/_client/hooks/server-action-hooks'
+import { blo } from 'blo'
 
 type UserItem = Pick<Tables<'users'>, 'avatar' | 'displayName'> | null
 
 export default function UserInfo({ initialUserInfo }: { initialUserInfo?: UserItem }) {
-    const {
-        data: userInfo,
-        isLoading,
-        isSuccess,
-    } = useServerActionQuery(getUserInfoAction, {
-        queryKey: ['userInfo'],
+    const { data: userInfo, isLoading } = useServerActionQuery(getUserInfoAction, {
+        queryKey: ['getUserInfoAction'],
         initialData: initialUserInfo || undefined,
         input: undefined,
     })
 
-    const account = useAccount()
+    const { address } = useAccount()
 
-    const truncatedAddress = account.address?.slice(0, 6) + '...' + account.address?.slice(-4)
+    const truncatedAddress = address?.slice(0, 6) + '...' + address?.slice(-4)
 
     return (
         <section className='detail_card inline-flex w-full gap-[0.69rem] rounded-3xl p-6'>
             <Avatar className='size-14 cursor-pointer' aria-label='User Avatar'>
                 {isLoading ? (
                     <Skeleton className='size-14 rounded-full' />
-                ) : userInfo?.avatar ? (
-                    <AvatarImage alt='User Avatar' src={userInfo.avatar} />
                 ) : (
-                    <ReactBlockies seed={account.address || '0x'} size={14} />
+                    <AvatarImage alt='user avatar' src={userInfo.avatar || blo(address || '0x')} />
                 )}
             </Avatar>
             <div className='flex-1 space-y-1'>
@@ -43,10 +37,10 @@ export default function UserInfo({ initialUserInfo }: { initialUserInfo?: UserIt
                     {isLoading ? <Skeleton className='h-4 w-24' /> : userInfo?.displayName || 'Anon User'}
                 </h1>
                 <h2 className='font-mono text-xs text-[#5472E9]'>
-                    {account.address ? truncatedAddress : <Skeleton className='h-4 w-16 bg-[#5472E9]/20' />}
+                    {address ? truncatedAddress : <Skeleton className='h-4 w-16 bg-[#5472E9]/20' />}
                 </h2>
             </div>
-            <Link href={`https://sepolia.basescan.org/address/${account.address}`} passHref legacyBehavior>
+            <Link href={`https://sepolia.basescan.org/address/${address}`} passHref legacyBehavior>
                 <a target='_blank' rel='external noopener noreferrer nofollow' className='self-center'>
                     <ExternalLinkIcon className='size-6' />
                 </a>
--- src/app/@topbar/_components/user-avatar.tsx
+++ src/app/@topbar/_components/user-avatar.tsx
@@ -2,33 +2,22 @@
 
 import { Avatar, AvatarImage } from '@/app/_components/ui/avatar'
 import { useUserAvatar } from '@/hooks/useUserAvatar'
-import { Route } from 'next'
 import Link from 'next/link'
-import ReactBlockies from 'react-blockies'
 import { useAccount } from 'wagmi'
+import { blo } from 'blo'
 
 export default function UserAvatar() {
-    const { data: userAvatar, isLoading, isError } = useUserAvatar()
+    const { data: userAvatar, isLoading } = useUserAvatar()
     const { address } = useAccount()
 
     if (isLoading) {
         return <div className='size-10 animate-pulse rounded-full bg-gray-200' />
     }
 
-    if (isError || !userAvatar) {
-        return (
-            <Avatar className='size-10 cursor-pointer' aria-label='Go to Profile' asChild>
-                <Link href={'/profile' as Route}>
-                    <ReactBlockies seed={address || '0x'} size={10} />
-                </Link>
-            </Avatar>
-        )
-    }
-
     return (
         <Avatar className='size-10 cursor-pointer' aria-label='Go to Profile' asChild>
-            <Link href={'/profile' as Route}>
-                <AvatarImage alt='User Avatar' src={userAvatar} />
+            <Link href={'/profile'}>
+                <AvatarImage alt='user avatar' src={userAvatar || blo(address || '0x')} />
             </Link>
         </Avatar>
     )
