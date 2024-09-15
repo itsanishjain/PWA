23d50a081971b035f36f68d1377e8c5592edd5ff
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-16 01:49:41 +0200

    fix: update and correct winner detail hook

---
+++ src/app/(pages)/pool/[pool-id]/_components/use-winner-detail.tsx
@@ -0,0 +1,16 @@
+import { getConfig } from '@/app/_client/providers/configs/wagmi.config'
+import { getWinnerDetail } from '@/app/_lib/blockchain/functions/pool/get-winner-detail'
+import { useQuery } from '@tanstack/react-query'
+
+export const useWinnerDetail = (poolId: bigint, address: string) => {
+    const {
+        data: winnerDetail,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ['get-winner-detail', poolId, address, getConfig().state.chainId],
+        queryFn: getWinnerDetail,
+    })
+
+    return { winnerDetail, isLoading, error }
+}
---
+++ src/app/_lib/blockchain/functions/pool/get-winner-detail.ts
@@ -0,0 +1,20 @@
+import { getConfig } from '@/app/_client/providers/configs/wagmi.config'
+import { currentPoolAddress } from '@/app/_server/blockchain/server-config'
+import { poolAbi } from '@/types/contracts'
+import { getPublicClient } from '@wagmi/core'
+import { Address } from 'viem'
+
+export const getWinnerDetail = async ({ queryKey }: { queryKey: [string, bigint, string, number] }) => {
+    const publicClient = getPublicClient(getConfig())
+
+    const [_, poolId, address] = queryKey
+
+    const winnerDetailFromSC = await publicClient?.readContract({
+        abi: poolAbi,
+        functionName: 'getWinnerDetail',
+        address: currentPoolAddress,
+        args: [poolId, address as Address],
+    })
+
+    return { winnerDetailFromSC }
+}
