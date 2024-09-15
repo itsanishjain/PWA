3c1683f8ca57f7417dfcc6a1fcd8e08bf704b003
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-16 01:30:20 +0200

    fix: improve page title styles and handling, still having issues in ssr

--- src/app/@topbar/_components/page-title.tsx
+++ src/app/@topbar/_components/page-title.tsx
@@ -1,20 +1,22 @@
 'use client'
 
+import { useAppStore } from '@/app/_client/providers/app-store.provider'
 import PoolTopLogo from '@/app/_components/icons/pool-top-logo'
-import { Route } from 'next'
 import Link from 'next/link'
-import { useAppStore } from '../../_client/providers/app-store.provider'
 
 export default function PageTitle() {
-    const topBarTitle = useAppStore(state => state.topBarTitle)
+    const title = useAppStore(s => s.topBarTitle)
 
     return (
-        <Link href={'/' as Route}>
-            <h1 className='text-[0.875rem] font-semibold'>{topBarTitle ? topBarTitle : <PoolTopLogo />}</h1>
+        <Link href='/'>
+            <h1 className='text-[0.875rem] font-semibold'>{title ?? <PoolTopLogo />}</h1>
         </Link>
     )
-    // Alternative with text instead of svg/png:
-    // <h1 className='text-center text-[42px] font-bold text-shadow-inner font-logo bg-clip-text text-transparent bg-text-inner'>
-    // 	pool
-    // </h1>
 }
+
+// TODO: fix SSR issue: title not getting set
+
+// Alternative with text instead of svg/png:
+// <h1 className='text-center text-[42px] font-bold text-shadow-inner font-logo bg-clip-text text-transparent bg-text-inner'>
+// 	pool
+// </h1>
