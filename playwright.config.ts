43a69f09f8cbc421fa1d7e57e866e0b797b63e15
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-16 02:18:23 +0200

    test: config playwright for mobile and enable ssl

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:3000',
+        baseURL: 'https://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -33,30 +33,30 @@ export default defineConfig({
 
     /* Configure projects for major browsers */
     projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        },
+        // {
+        //   name: 'chromium',
+        //   use: { ...devices['Desktop Chrome'] },
+        // },
 
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
 
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //   name: 'webkit',
+        //   use: { ...devices['Desktop Safari'] },
+        // },
 
         /* Test against mobile viewports. */
-        // {
-        //   name: 'Mobile Chrome',
-        //   use: { ...devices['Pixel 5'] },
-        // },
-        // {
-        //   name: 'Mobile Safari',
-        //   use: { ...devices['iPhone 12'] },
-        // },
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'] },
+        },
+        {
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 13'] },
+        },
 
         /* Test against branded browsers. */
         // {
@@ -71,8 +71,8 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     // webServer: {
-    //     command: 'pnpm run start',
-    //     url: 'https://127.0.0.1:3000',
+    //     command: 'npm run dev',
+    //     url: 'http://app.localhost:3000',
     //     reuseExistingServer: !process.env.CI,
     // },
 })
