6c53c47b010fedca92c79d888364802c8b9e3c24
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-27 11:13:49 +0100

    fix: repair issue with wrong zustand version. Remove state from providers

--- src/app/_client/providers/configs/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as privy } from './privy.config'
--- src/app/_client/providers/providers.tsx
+++ src/app/_client/providers/providers.tsx
@@ -1,19 +1,14 @@
-/**
- * @file src/providers/providers.tsx
- * @description the main providers for the application
- */
 'use client'
 
 import { PrivyProvider } from '@privy-io/react-auth'
 import { WagmiProvider } from '@privy-io/wagmi'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Toaster } from 'sonner'
-import { privy } from './configs'
 import { AppStoreProvider } from './app-store.provider'
-import { useState } from 'react'
 import { getConfig } from './configs/wagmi.config'
 import { cookieToInitialState } from 'wagmi'
 import ConfiguredQueryProvider from './query'
+import privy from './configs/privy.config'
 
 type Props = {
     children: React.ReactNode
@@ -21,7 +16,7 @@ type Props = {
 }
 
 export default function Providers({ children, cookie }: Props) {
-    const [config] = useState(() => getConfig())
+    const config = getConfig()
     const initialState = cookieToInitialState(config, cookie)
 
     return (
