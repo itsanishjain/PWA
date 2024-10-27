a2fb5f11d8dd08f1ed776ce2ceb1c57cc07a92ac
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-10-27 05:12:00 +0100

    fix: adjust token decimals for USDC

--- src/app/(pages)/pool/new/use-create-pool.ts
+++ src/app/(pages)/pool/new/use-create-pool.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { useFormState } from 'react-dom'
 import { useRouter } from 'next/navigation'
-import { parseEther, Hash, parseEventLogs, ContractFunctionExecutionError } from 'viem'
+import { parseEther, Hash, parseEventLogs, ContractFunctionExecutionError, formatUnits } from 'viem'
 import { createPoolAction, deletePool, updatePoolStatus } from './actions'
 import { Steps, usePoolCreationStore } from '@/app/_client/stores/pool-creation-store'
 import { useWaitForTransactionReceipt } from 'wagmi'
@@ -72,7 +72,7 @@ export function useCreatePool() {
                 BigInt(startDate / 1000), // is important to convert to seconds
                 BigInt(endDate / 1000),
                 name,
-                parseEther(price),
+                formatUnits(BigInt(price), 6),
                 1000, // TODO: implement max participants
                 currentTokenAddress,
             ],
