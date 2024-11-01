4d32076b515e786d0767a75fbd2e067839f010b2
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-11-01 17:02:01 +0700

    fix: remove loaders causing random stuck indicators

--- src/app/(pages)/pool/[pool-id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderIcon } from 'lucide-react'
-
-export default function PoolDetailsLoader() {
-    return (
-        <div className='flex flex-1 items-center justify-center'>
-            <LoaderIcon className='animate-spin' />
-        </div>
-    )
-}
--- src/app/(pages)/pools/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderIcon } from 'lucide-react'
-
-export default function PoolsLoader() {
-    return (
-        <div className='flex flex-1 items-center justify-center'>
-            <LoaderIcon className='animate-spin' />
-        </div>
-    )
-}
