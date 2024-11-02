8d2ce53c49df5495a5afd4c7f46d3ad3cdc1030b
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-11-03 01:22:10 +0700

    fix: wrap send page into page wrapper to correct styling

--- src/app/(pages)/profile/send/page.tsx
+++ src/app/(pages)/profile/send/page.tsx
@@ -1,11 +1,18 @@
+import PageWrapper from '@/components/page-wrapper'
 import AmountSection from './_components/amount-section'
 import ProfileBalanceSection from './_components/balance-section'
 
 export default function ProfileLayout() {
     return (
-        <div className='flex flex-1 flex-col gap-6'>
-            <ProfileBalanceSection />
-            <AmountSection />
-        </div>
+        <PageWrapper
+            topBarProps={{
+                backButton: true,
+                title: 'Send',
+            }}>
+            <div className='flex flex-1 flex-col gap-6'>
+                <ProfileBalanceSection />
+                <AmountSection />
+            </div>
+        </PageWrapper>
     )
 }
